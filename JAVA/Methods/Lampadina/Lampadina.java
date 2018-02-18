07305fdeda9a8c324f007f771022db2f1dec546c
Un-knownCoder <34581283+Un-knownCoder@users.noreply.github.com>
2018-02-18 14:15:05 +0100

    Update Lampadina.java

--- JAVA/Methods/Lampadina/Lampadina.java
+++ JAVA/Methods/Lampadina/Lampadina.java
@@ -13,10 +13,12 @@ public class Lampadina {
 	}
 	
 	public void click(){
+		// modifica lo stato della lampadina
 		stato = mClicks > 0 ? 2 : stato > 0 ? 0 : 1;
 	}
 	
 	public void stato(){
+		// Stampa lo stato attuale della lampadina
 		String s = stato < 1 ? "spenta" : stato > 1 ? "rotta" : "accesa";
 		System.out.println(s);
 	}
