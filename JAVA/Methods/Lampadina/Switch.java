65bc3f973dfc5340abda2074e12e683e9828a08f
Un-knownCoder <34581283+Un-knownCoder@users.noreply.github.com>
2018-02-19 18:02:26 +0100

    Create Switch.java

---
+++ JAVA/Methods/Lampadina/Switch.java
@@ -0,0 +1,16 @@
+package Lampadina;
+
+public class Switch {
+  private Lampadina lamp;
+  
+  public Switch(mLClicks) {
+    lamp = new Lampadina(mLClicks);
+  }
+  
+  public void changeState(){
+    lamp.click();
+    int st = lamp.getState();
+    
+    String state = s < 1 ? "spenta" : s > 1 ? "rotta" : "accesa";
+  }
+}	
